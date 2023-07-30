7521fc94e9ad06e527d0454b832d4227721457cf
Andrew Martin <114.amart@gmail.com>
2023-07-30 11:18:27 -0200

    workflows: removed original workflow files

--- mplgz2ingested/workflows/calibrate_day.py
+++
@@ -1,5 +0,0 @@
-'''Author: Andrew Martin
-Creation Date: 30/6/23
-
-Function to calibrate a day of pre-ingested data.
-'''
--- mplgz2ingested/workflows/ingest_day.py
+++
@@ -1,5 +0,0 @@
-'''Author: Andrew Martin
-Creation Date: 30/6/23
-
-Script to allow the ingestion and calibration of a day's worth of MPL data.
-'''
